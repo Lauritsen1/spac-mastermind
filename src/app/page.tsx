b3a3affabdc808d70f2945c718934665416d295a
Rasmus Lauritsen <43439056+Lauritsen1@users.noreply.github.com>
2024-02-12 15:49:37 +0100

    add generateRandomCode function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function page() {
-  return <div>page</div>
+"use client"
+
+import { useState, useEffect } from "react"
+
+export default function Page() {
+  const [code, setCode] = useState<string[]>([])
+
+  const generateRandomCode = () => {
+    const PEGS: string[] = ["red", "yellow", "green", "blue", "white", "black"]
+    const newArray: string[] = []
+    for (let i = 0; i < 4; i++) {
+      const randomNumber = Math.floor(Math.random() * PEGS.length)
+      newArray.push(PEGS[randomNumber])
+    }
+    setCode(newArray)
+  }
+
+  useEffect(() => {
+    generateRandomCode()
+  }, [])
+
+  return (
+    <div>
+      {code.map((item, i) => (
+        <div key={i}>{item}</div>
+      ))}
+    </div>
+  )
 }

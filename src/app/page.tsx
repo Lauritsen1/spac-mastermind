b887f1a923a349e07004f3635f73fe677bddf56d
Rasmus Lauritsen <rasmus.lauritsen.dev@outlook.com>
2024-02-13 11:27:44 +0100

    add color picker UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
   }, [])
 
   return (
-    <div className="grid place-content-center h-screen">
+    <div className="flex justify-center h-screen pt-4">
       <div>
         <div className="flex justify-between items-center border-b mb-4 pb-4">
           <h2 className="font-bold text-3xl">Mastermind</h2>
@@ -37,25 +37,34 @@ export default function Page() {
             <ThemeToggle />
           </div>
         </div>
-        <div className="max-w-max space-y-4">
+        <div className="flex flex-col-reverse gap-4 max-w-max">
           {Array(12)
             .fill(null)
             .map((_, i) => (
-              <div key={i} className="flex items-center gap-8">
-                <div className="flex gap-4 *:rounded-full *:border-4 *:h-12 *:w-12">
+              <div key={i} className="flex justify-between items-center gap-8">
+                <span className="text-muted-foreground">{i + 1}.</span>
+                <div className="flex gap-4 *:rounded-full *:border-4 *:h-10 *:w-10">
                   <div></div>
                   <div></div>
                   <div></div>
                   <div></div>
                 </div>
-                <div className="flex gap-2 *:rounded-full *:border-2 *:h-4 *:w-4">
-                  <div></div>
-                  <div></div>
-                  <div></div>
+                <div className="flex gap-2 *:rounded-full *:border-[3px] *:h-4 *:w-4">
+                  <div className="bg-green-500 "></div>
+                  <div className="bg-green-500"></div>
+                  <div className="bg-yellow-500"></div>
                   <div></div>
                 </div>
               </div>
             ))}
+        </div>
+        <div className="flex justify-center gap-4 mt-4 pt-4 border-t *:rounded-full *:border-4 *:h-10 *:w-10">
+          <div className="bg-red-500"></div>
+          <div className="bg-yellow-500"></div>
+          <div className="bg-green-500"></div>
+          <div className="bg-blue-500"></div>
+          <div className="bg-orange-500"></div>
+          <div className="bg-purple-500"></div>
         </div>
       </div>
     </div>
